2139dbfe376dfee55e697215d236bbcf142c55bb
liangchiwen <liangchiwen@viigaa.com>
2014-08-15 09:32:53 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ViigaaWifi::Application.config.secret_key_base = '09a06a0efe8a7deb1757c0b6de923dea6f26ebb93a4e2309efa0c88a1a1a52b627d6646b41da2254daee94e2c3a808aed7adfa599b7b8ee7915e51bf006f1f24'
